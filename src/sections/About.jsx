c00ec11a98efc5d680ff53448a5abf171f43d99a
natbates <nathaniel.christopher.bates@gmail.com>
2024-12-22 17:40:57 +0000

    new design

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -16,7 +16,7 @@ const About = () => {
             </div>
             <div id = "about-text">
                 <h1>Attract new leads, re-engage customers or reach a new <span className = "yellow-underline">target</span> market with email marketing, SEO blogs or new site content, whitepapers or guides</h1>
-                <p>After leaving university, where I studied journalism and media, I started writing funny pieces for local magazines. I became a professional content writer in 2008 working for the UK's largest outdoor brand. I then moved into marketing management with a range of PLCs and tech companies. In 2016 I became the editor of Incentive&Motivation magazine, and after the birth of my one and only child, I became a full-time freelance B2B content writer.</p>
+                <p>After leaving university, where I studied journalism and media, I started writing funny pieces for local magazines. I became a professional content writer in 2008 working for the UK's largest outdoor brand. I then moved into marketing management with a range of PLCs and tech companies. In 2016 I became the editor of Incentive & Motivation magazine, and after the birth of my one and only child, I became a full-time freelance B2B content writer.</p>
                 <button>Find out more</button>
                 <div className = "texture-1"></div>
             </div>
